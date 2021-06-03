429a33214a8a93a60e3df70b755357c018b89baa
Kyle Rogers <77065855+KyleRogers641@users.noreply.github.com>
2021-06-03 14:58:20 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,7 +8,9 @@ function carousel() {
     x[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 5000); 
 }
@@ -23,8 +25,12 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  } 
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
